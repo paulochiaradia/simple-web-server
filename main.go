9afbd44928222a79af2483cc490e569da48761fc
Paulo Roberto Chiardia Neto <97291302+paulochiaradia@users.noreply.github.com>
2024-10-29 15:05:24 -0300

    Refactor code to add HTTP handlers for "/hello" and "/form" routes, and serve static files
    This commit refactors the code in main.go to add two new HTTP handlers for the "/hello" and "/form" routes. It also includes the necessary logic to serve static files using the http.FileServer. Additionally, two new HTML files, form.html and index.html, are added to the static directory.

--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	//check if the current request URL Path matches "/"
+	if r.URL.Path != "/hello" {
+		http.NotFound(w, r)
+		return
+	}
+
+	//Using r.Method to check whether the request is using GET or not
+	if r.Method != "GET" {
+		r.Header.Set("Allow", http.MethodGet)
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	w.Write([]byte("hello!"))
+}
+
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	//check the parsed form
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+
+	//print the form data
+	fmt.Fprintf(w, "POST request successful!")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+	w.Write([]byte("\n"))
+	w.Write([]byte(name))
+	w.Write([]byte(address))
+
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/form", formHandler)
+
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
